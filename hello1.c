4582a052d474c54b6aa7b717e73f0bd9ed165024
Klaudiusz Kowalczyk <klaudi1234@o2.pl>
2025-01-24 10:18:06 +0100

    dodanie menu

--- hello1.c
+++ hello1.c
@@ -10,12 +10,23 @@ float sphere_volume(float r)
 return((4.0/3.0)*3.141592*(r*r*r));
 }
 
-int main () {
 
+
+int main () {
  float a, b, h, r;
-/*
-printf("Podaj dlugosc podstawy prostopadloscianu: ");
-scanf("%f", &a);
+int command;
+
+while (1) {
+    printf("1 - objetosc prostopadloscionu \n");
+    printf("2 - objetosc kuli \n");
+    printf("0 - wyjscie \n");
+
+scanf("%id", &command);
+
+if (command==1)
+{
+    printf("Podaj dlugosc podstawy prostopadloscianu: ");
+    scanf("%f", &a);
 
 printf("Podaj szerokosc podstawy prostopadloscianu: ");
 scanf("%f", &b);
@@ -24,13 +35,22 @@ printf("Podaj wysokosc podstawy prostopadloscianu: ");
 scanf("%f", &h);
 
 printf("\n Objetosc prostopadloscianu: %f \n", cuboid_volume(a, b, h));
-*/
-
-printf("Podaj promien kuli: ");
+}
+else if(command==2)
+{
+    printf("Podaj promien kuli: ");
 scanf("%f", &r);
 
 printf("\n Objetosc kuli: %f \n", sphere_volume(r));
+}
+else if(command==0)
+{
+    printf("Koniec dzialania programu \n");
+    return(0);
 
-return(0);
-
+}
+else{
+    printf("nieprawidlowy numer polecenia \n");
+}
+}
 }
