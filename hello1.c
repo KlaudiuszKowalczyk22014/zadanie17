6c8af1a0ad6e6145658b53b5d63c0ecda50ab5b8
Klaudiusz Kowalczyk <klaudi1234@o2.pl>
2025-01-24 09:35:10 +0100

    Objetosc kuli

--- hello1.c
+++ hello1.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
-float cuboid_volume(float a, float b, float h)
+ float cuboid_volume(float a, float b, float h)
 {
 return(a*b*h);
 }
 
+float sphere_volume(float r)
+{
+return((4.0/3.0)*3.14*(r*r*r));
+}
+
 int main () {
 
-float a, b, h;
-
+ float a, b, h, r;
+/*
 printf("Podaj dlugosc podstawy prostopadloscianu: ");
 scanf("%f", &a);
 
@@ -19,6 +24,13 @@ printf("Podaj wysokosc podstawy prostopadloscianu: ");
 scanf("%f", &h);
 
 printf("\n Objetosc prostopadloscianu: %f \n", cuboid_volume(a, b, h));
+*/
+
+printf("Podaj promien kuli: ");
+scanf("%f", &r);
+
+printf("\n Objetosc kuli: %f \n", sphere_volume(r));
 
 return(0);
+
 }
